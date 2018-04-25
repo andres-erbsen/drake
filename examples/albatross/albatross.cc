4469861d9022ae7f2586845f3fb15e61bccb1b6c
Andres Erbsen <andreser@mit.edu>
2018-04-25 13:50:12 +0000

    albatross: model wind

--- examples/albatross/albatross.cc
+++ examples/albatross/albatross.cc
@@ -13,7 +13,7 @@ template <typename T>
 Albatross<T>::Albatross()
 : systems::LeafSystem<T>(systems::SystemTypeTag<albatross::Albatross>{}) {
   // state: speed, pitch, yaw
-  this->DeclareContinuousState(3, 0, 0);
+  this->DeclareContinuousState(4, 0, 0);
   // input: lift coefficient, roll angle
   this->DeclareVectorInputPort(AlbatrossInput<T>());
 }
@@ -30,16 +30,17 @@ void Albatross<T>::DoCalcTimeDerivatives(
   // TODO: parametrize
   // DS Kinetic 60
   const double m = 2;
-  const double g = 10;
+  const double g = 9.8;
   const double cD0 = .005;
   const double k = .08;
   const double S = .23;
   const double rho = 1; // TODO real value
+  const double windspeed_gradient = 0; // .02 bottom edge of jetstrem at 5km..
 
   const T speed = context.get_continuous_state().get_generalized_position().GetAtIndex(0);
   const T pitch = context.get_continuous_state().get_generalized_position().GetAtIndex(1);
   const T yaw   = context.get_continuous_state().get_generalized_position().GetAtIndex(2);
-  //const T altitude = context.get_continuous_state().get_generalized_position().GetAtIndex(3);
+  //unused: const T altitude = context.get_continuous_state().get_generalized_position().GetAtIndex(3);
 
   const T cL = this->EvalVectorInput(context, 0)->GetAtIndex(0);
   const T roll = this->EvalVectorInput(context, 0)->GetAtIndex(1);
@@ -49,12 +50,12 @@ void Albatross<T>::DoCalcTimeDerivatives(
   const T L = .5*cL*rho*S*speed*speed;
 
   const T altitude_dot = speed*sin(pitch);
-  const T Wd = 0*altitude_dot;
+  const T Wd = windspeed_gradient*altitude_dot;
 
   derivatives->get_mutable_generalized_position().SetAtIndex(0, 1/(m)*(                              -D      - m*g*sin(pitch) + m*Wd*cos(pitch)*sin(yaw)));
   derivatives->get_mutable_generalized_position().SetAtIndex(1, 1/(m*(.0001+speed))*(            L*cos(roll) - m*g*cos(pitch) - m*Wd*sin(pitch)*sin(yaw)));
   derivatives->get_mutable_generalized_position().SetAtIndex(2, 1/(m*(.0001+speed)*cos(pitch))*( L*sin(roll)                  + m*Wd           *cos(yaw)));
-  //derivatives->get_mutable_generalized_position().SetAtIndex(3, altitude_dot);
+  derivatives->get_mutable_generalized_position().SetAtIndex(3, altitude_dot);
 }
 
 }  // namespace albatross
--- examples/albatross/trajectory_optimization.cc
+++ examples/albatross/trajectory_optimization.cc
@@ -15,25 +15,28 @@ int trajectory_optimization() {
   auto albatross = std::make_unique<Albatross<double>>();
   auto context = albatross->CreateDefaultContext();
 
-  const int N = 41;
+  const int N = 11;
   const double dt_min = .1;
-  const double dt_max = .2;
+  const double dt_max = .1;
 
   systems::trajectory_optimization::DirectCollocation dircol(
       albatross.get(), *context, N, dt_min, dt_max);
   dircol.AddEqualTimeIntervalsConstraints();
 
-  const Eigen::Vector3d initial_state(25, 0, 0);
+  const Eigen::Vector4d initial_state(25, 0, 0, 0);
   dircol.AddLinearConstraint(dircol.initial_state() == initial_state);
 
   dircol.AddConstraintToAllKnotPoints(dircol.state()(0) >= 15);
   dircol.AddConstraintToAllKnotPoints(dircol.state()(0) <= 35);
 
-  dircol.AddConstraintToAllKnotPoints(dircol.state()(1) >= 0);
-  dircol.AddConstraintToAllKnotPoints(dircol.state()(1) <= 1);
+  dircol.AddConstraintToAllKnotPoints(dircol.state()(1) >= -acos(0.));
+  dircol.AddConstraintToAllKnotPoints(dircol.state()(1) <= acos(0.));
 
-  dircol.AddConstraintToAllKnotPoints(dircol.state()(2) >= -acos(0.));
-  dircol.AddConstraintToAllKnotPoints(dircol.state()(2) <= acos(0));
+  dircol.AddConstraintToAllKnotPoints(dircol.state()(2) >= -6*acos(0.));
+  dircol.AddConstraintToAllKnotPoints(dircol.state()(2) <= 6*acos(0.));
+
+  dircol.AddConstraintToAllKnotPoints(dircol.state()(3) >= -100);
+  dircol.AddConstraintToAllKnotPoints(dircol.state()(3) <= 100);
 
   dircol.AddConstraintToAllKnotPoints(dircol.input()(0) >= 0);
   dircol.AddConstraintToAllKnotPoints(dircol.input()(0) <= 1.2);
@@ -41,23 +44,60 @@ int trajectory_optimization() {
   dircol.AddConstraintToAllKnotPoints(dircol.input()(1) >= -acos(0.));
   dircol.AddConstraintToAllKnotPoints(dircol.input()(1) <= acos(0.));
 
-  dircol.AddRunningCost(.001*dircol.input()(0)*dircol.input()(0));
-  dircol.AddFinalCost(100-dircol.state()(0));
-
   printf("num_vars = %d\n", dircol.num_vars());
 
-  auto result = dircol.Solve();
-  if (result != solvers::SolutionResult::kSolutionFound) {
-    fprintf(stderr, "optimization failed!\n");
-    return 1;
+  {
+    auto result = dircol.Solve();
+    if (result != solvers::SolutionResult::kSolutionFound) {
+      fprintf(stderr, "solving failed!\n");
+      return 1;
+    }
+
+    auto inputs = dircol.ReconstructInputTrajectory();
+    auto traj = dircol.ReconstructStateTrajectory();
+    auto timestamps = traj.get_segment_times();
+    for (size_t i = 0; i < timestamps.size(); i++) {
+      auto t = timestamps[i];
+      auto V = traj.value(timestamps[i]).coeff(0);
+      auto pitch = traj.value(timestamps[i]).coeff(1);
+      auto yaw = traj.value(timestamps[i]).coeff(2);
+      auto altitude = traj.value(timestamps[i]).coeff(3);
+      auto cL = inputs.value(timestamps[i]).coeff(0);
+      auto roll = inputs.value(timestamps[i]).coeff(1);
+      printf("%3.3f: %3.3f %3.3f %3.3f %3.3f <<- %3.3f %3.3f | %3.3f\n", t, V, pitch, yaw, altitude, cL, roll, 9.8*altitude + .5*V*V);
+    }
+    printf("\n");
   }
 
-  auto inputs = dircol.ReconstructInputTrajectory();
-  auto traj = dircol.ReconstructStateTrajectory();
-  auto t = traj.get_segment_times();
-  for (size_t i = 0; i < t.size(); i++) {
-    printf("%3.3f: %3.3f %3.3f %3.3f <<- %3.3f %3.3f\n", t[i], traj.value(t[i]).coeff(0), traj.value(t[i]).coeff(1), traj.value(t[i]).coeff(2), inputs.value(t[i]).coeff(0), inputs.value(t[i]).coeff(1));
+  {
+    dircol.SetInitialTrajectory(dircol.ReconstructInputTrajectory(), dircol.ReconstructStateTrajectory());
+
+    dircol.AddRunningCost(.00001*dircol.input()(0)*dircol.input()(0));
+    dircol.AddRunningCost(.0001*dircol.input()(1)*dircol.input()(1));
+    dircol.AddFinalCost(-(9.8*dircol.state()(3) + .5*dircol.state()(0)*dircol.state()(0)) ); // m(gh + .5vv)
+
+    auto result = dircol.Solve();
+    if (result != solvers::SolutionResult::kSolutionFound) {
+      fprintf(stderr, "optimization failed!\n");
+      return 1;
+    }
+
+    auto inputs = dircol.ReconstructInputTrajectory();
+    auto traj = dircol.ReconstructStateTrajectory();
+    auto timestamps = traj.get_segment_times();
+    for (size_t i = 0; i < timestamps.size(); i++) {
+      auto t = timestamps[i];
+      auto V = traj.value(timestamps[i]).coeff(0);
+      auto pitch = traj.value(timestamps[i]).coeff(1);
+      auto yaw = traj.value(timestamps[i]).coeff(2);
+      auto altitude = traj.value(timestamps[i]).coeff(3);
+      auto cL = inputs.value(timestamps[i]).coeff(0);
+      auto roll = inputs.value(timestamps[i]).coeff(1);
+      printf("%3.3f: %3.3f %3.3f %3.3f %3.3f <<- %3.3f %3.3f | %3.3f\n", t, V, pitch, yaw, altitude, cL, roll, 9.8*altitude + .5*V*V);
+    }
+    printf("\n");
   }
+
 
   return 0;
 }
